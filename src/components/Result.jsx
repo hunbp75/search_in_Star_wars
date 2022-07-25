2a9aabd128a39e7b32c9f5c6e01acd512642f1e5
hunbp75 <hunbp75@gmail.com>
2022-07-25 15:32:11 +0200

    add Result page css

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
-import "../App.css";
+import "./result.css";
 
 const Result = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const ide = searchParams.get("id");
+  const id = searchParams.get("id");
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const Result = () => {
       });
   }, []);
 
-  console.log(ide);
+  console.log(id);
 
   const goBack = () => {
     navigate("/");
@@ -28,19 +28,33 @@ const Result = () => {
       <h1>Star Wars Character</h1>
 
       {data
-        .filter((value) => value.id == ide)
+        .filter((value) => value.id == id)
         .map((value, key) => {
           return (
-            <div key={ide} className="container">
-              <h4>Name: {value.name}</h4>
-              <h5>Gender: {value.gender}</h5>
-              <h5>HomeWorld: {value.homeworld}</h5>
-              <img src={value.image} alt="" />
+            <div key={id} className="container">
+              <div className="leftSide">
+                <h2>Name: {value.name}</h2>
+                <h4>Gender: {value.gender}</h4>
+                <h4>HomeWorld: {value.homeworld}</h4>
+                <h4>Height: {value.height} cm</h4>
+                <h4>Mass: {value.mass} kg</h4>
+                <h4>Born: {value.born}</h4>
+                <h4>Born Location: {value.bornLocation}</h4>
+                <h4>Died: {value.died}</h4>
+                <h4>Died Location: {value.diedLocation}</h4>
+                <br></br>
+                <a href={value.wiki}>Star Wars Wiki Page: </a>
+              </div>
+              <div className="rightSide">
+                <img src={value.image} alt="" />
+              </div>
             </div>
           );
         })}
 
-      <button onClick={goBack}>Back to Home</button>
+      <button onClick={goBack} className="goBack-btn">
+        Back to Home
+      </button>
     </div>
   );
 };
