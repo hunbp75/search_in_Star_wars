184ee823526f67b3b06c3051b0ac430d9cb8854a
hunbp75 <hunbp75@gmail.com>
2022-08-17 10:57:28 +0200

    add pictures to the search-bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,8 +7,11 @@ import "./search.css";
 const Search = () => {
   const [starWarsData, setStarWarsData] = useState([]);
   const [filterData, setFilterData] = useState([]);
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState();
+  const [selectedName, setSelectedName] = useState("");
   const navigate = useNavigate();
+
+  console.log("idSelected: ", selected);
 
   // Api adatok lekérése
   useEffect(() => {
@@ -41,42 +44,62 @@ const Search = () => {
     });
   };
 
-  console.log(selected);
-
   return (
-    <div className="search">
-      <h1>Star Wars Character - Search</h1>
-      <div className="input-container">
-        <input
-          className="searchInput"
-          type="text"
-          placeholder="typing the name"
-          autoComplete="on"
-          onChange={handleFilter}
-        />
-        <button type="submit" onClick={onClick} className="search-btn">
-          Search
-        </button>
+    <div>
+      {/* Search bar */}
+      <div className="search">
+        <h1>Star Wars Character - Search</h1>
+        <div className="input-container">
+          <input
+            className="searchInput"
+            type="text"
+            placeholder={selectedName ? selectedName : "typing the name"}
+            autoComplete="on"
+            onChange={handleFilter}
+          />
+          <button type="submit" onClick={onClick} className="search-btn">
+            Click
+          </button>
+        </div>
+        {filterData.length !== 0 && (
+          <div className="dataResult">
+            {filterData.map((item) => {
+              return (
+                <div key={item.id}>
+                  <div
+                    className="dataItem"
+                    onClick={() => {
+                      setSelected(item.id);
+                      setSelectedName(item.name);
+                    }}
+                  >
+                    {item.name}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
 
-      {filterData.length !== 0 && (
-        <div className="dataResult">
-          {filterData.map((item) => {
-            return (
-              <div key={item.id}>
-                <div
-                  className="dataItem"
-                  onClick={() => {
-                    setSelected([item.id]);
-                  }}
-                >
-                  {item.name}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+      {/* pictures on the page */}
+      <div className="searchPicturesMainContainer">
+        {filterData.map((pictures) => {
+          return (
+            <div className="searchPicturesContainer" key={pictures.id}>
+              <p>{pictures.name}</p>
+              <img
+                className="searchPictures"
+                src={pictures.image}
+                alt=""
+                onClick={() => {
+                  setSelected(pictures.id);
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
